58db96402ee5228924444bbc9be736231d8bda7f
mc-greq <greq19@gmail.com>
2019-01-27 18:00:50 +0100

    Reading object from database - with conditions

---
+++ src/hibernate/StudentEntity.java
@@ -0,0 +1,69 @@
+package hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "student", schema = "hb_student_tracker", catalog = "")
+public class StudentEntity {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentEntity that = (StudentEntity) o;
+        return id == that.id &&
+                Objects.equals(firstName, that.firstName) &&
+                Objects.equals(lastName, that.lastName) &&
+                Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email);
+    }
+}
---
+++ src/hibernate/demo/QueryStudentDemo.java
@@ -0,0 +1,56 @@
+package hibernate.demo;
+
+import hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+import java.util.Optional;
+
+public class QueryStudentDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        try(factory){
+            Session session = factory.getCurrentSession();
+
+            // start a transaction
+            session.beginTransaction();
+
+            // query the students
+            List<Student> studentsList = session.createQuery("from Student", Student.class)
+                    .getResultList();
+
+            // display the students
+            System.out.println("List of all students:");
+            studentsList.forEach(System.out::println);
+
+            // query students by last name
+            System.out.println("\nStudents queried by lastName:");
+            studentsList = session.createQuery("from Student s where s.lastName='Rutkowski'", Student.class)
+                    .getResultList();
+
+            // display query results
+            studentsList.forEach(System.out::println);
+
+            System.out.println("\nStudents queried by email:");
+            studentsList = session.createQuery("from Student s where s.email like '%mail.com'", Student.class)
+                    .getResultList();
+
+            studentsList.forEach(System.out::println);
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+        }
+
+    }
+}
